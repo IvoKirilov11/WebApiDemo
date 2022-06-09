8f54db28063c56fce4091a9a6deec59ca499d0d5
IvoKirilov11 <mefistofel_1980@abv.bg>
2022-06-09 20:15:18 +0300

    ChangeRange

--- WebApiDemo/WeatherForecast.cs
+++ WebApiDemo/WeatherForecast.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApiDemo
 {
     public class WeatherForecast
     {
         public DateTime Date { get; set; }
 
+        [Range (-100,100)]
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
